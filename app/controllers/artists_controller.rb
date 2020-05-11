bd40fa998d559a32b84d963aface8473c67e148f
dwanerich <dwanerich@gmail.com>
2020-05-11 17:44:03 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,16 @@
 class ArtistsController < ApplicationController
-  def index
-  end
-
+  
+  
   def show
+    @artist = Artist.find_by(id: params[:id])
+
+  end
+  
+  def index
+    @artists = Artist.all
   end
 
+  
   def new
     @artist = Artist.new
   end
